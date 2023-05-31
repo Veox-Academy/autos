1444072319846533d9e41f71fcb75be2cdc99c92
Sr.Einarr <valentinmakowiecki15@gmail.com>
2023-05-31 12:44:15 -0300

    .

---
+++ coches2/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "bodacious"
+game "gta5"
+
+client_script "data/tuning.lua"
+
+files {
+	"data/*",
+	"audio/*",
+	"audio/**/*"
+}
+
+data_file "HANDLING_FILE" "data/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
+data_file "CARCOLS_FILE" "data/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
+
+data_file "AUDIO_WAVEPACK" "audio/toysupmk4/dlc_toysupmk4"                                                                          
